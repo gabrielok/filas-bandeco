ba7c69002448577c1753e958830bf6e3368f93c7
gabrielok <gabrielokamoto@hotmail.com>
2017-11-05 20:01:32 +0000

    Amend application_controller

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,6 @@ module FilasBandeco
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
-    def homepage
-    render html: "Bem vindo!"
-    end
   end
   
   
