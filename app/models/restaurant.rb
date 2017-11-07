2653fe3b1dab405ba1f13915183704f90b0196a5
gabrielok <gabrielokamoto@hotmail.com>
2017-11-07 00:44:06 +0000

    Hotfix

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,4 @@
 class Restaurant < ApplicationRecord
-    validates(:name, presence: true), length: {maximum: 50}
-    validates(:email, presence: true), length: {maximum: 255}
+    validates(:name, presence: true, length: {maximum: 50})
+    validates(:email, presence: true, length: {maximum: 255})
 end
