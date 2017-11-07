61f682e1c50e2a3ca5349ce3a50cdefa208a8a7b
gabrielok <gabrielokamoto@hotmail.com>
2017-11-07 00:25:51 +0000

    Improve homepage

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     def homepage
-      render :test
+      render :homepage
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'restaurants/new'
-
   root 'application#homepage'
   resources :test
 end
