d89f715215cd08c3a7f61bb8e7b8fdfea1f143e2
gabrielok <gabrielokamoto@hotmail.com>
2017-11-12 21:47:17 +0000

    Finish Restaurant MVC

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,18 +1,11 @@
 class RestaurantsController < ApplicationController
-  def new
-  end
   def index
     @restaurant = Restaurant.all
   end
   def show
     @restaurant = Restaurant.find(params[:id])
   end
-  def quimica
-  end
-  def prefeitura
-  end
-  def central
-  end
-  def fisica
+  def info
+    @restaurant = Restaurant.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :restaurants
   root 'application#homepage'
+  get 'restaurants/info/:id', to: 'restaurants#info'
 end
