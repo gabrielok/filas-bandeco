73e21327483a65ca79b9de732402519170a03066
gabrielok <gabrielokamoto@hotmail.com>
2017-11-12 01:56:15 +0000

    Improve Homepage buttons

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
   resources :restaurants
-  # get 'restaurants/prefeitura'
-  # get 'restaurants/fisica'
-  # get 'restaurants/central'
-  # get 'restaurants/quimica'
   root 'application#homepage'
 end
