5347233e36a37215a52aeb9600049d6cfbec52f9
gabrielok <gabrielokamoto@hotmail.com>
2017-12-11 07:26:29 +0000

    Minor fixes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    redirect_to controller: 'users', action: 'index'
   end
   
   def create
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,7 +3,7 @@ class Restaurant < ApplicationRecord
     validates :name, presence: true, length: {maximum: 30}, uniqueness: true
     validates :menu, presence: true, length: {maximum: 100}, format: {with: URL_REGEX}
     # Restaurant.create(name: "Prefeitura", website: "http://sites.usp.br/sas/restaurante-da-prefeitura/", menu: "https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn=7")
-    # Restaurant.create(name: "Física", website: "http://sites.usp.br/sas/restaurante-da-fisica/", menu: "https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn=8")
+    # Restaurant.create(name: "Fisica", website: "http://sites.usp.br/sas/restaurante-da-fisica/", menu: "https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn=8")
     # Restaurant.create(name: "Central", website: "http://sites.usp.br/sas/restaurante-central/", menu: "https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn=6")
-    # Restaurant.create(name: "Química", website: "http://sites.usp.br/sas/restaurante-da-quimica/", menu: "https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn=9")
+    # Restaurant.create(name: "Quimica", website: "http://sites.usp.br/sas/restaurante-da-quimica/", menu: "https://uspdigital.usp.br/rucard/Jsp/cardapioSAS.jsp?codrtn=9")
 end
